57f2907f62f0d68e09cc2b394bdbe64418148c40
miltonarmando <miltonarmando@outlook.com>
2020-07-12 03:41:34 +0200

    'finalizacao'

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('sass/*.sass')
+  return gulp.src('sass/**/*.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('css'));
 });
- 
-gulp.task('watch', function () {
-  gulp.watch('sass/*.sass', ['sass']);
+
+gulp.task('default', function(done) {
+  done();
 });
 
-gulp.task('default', function(done){
-    done();
+gulp.task('watch', function () {
+  gulp.watch('sass/*.sass', ['sass']);
 });
